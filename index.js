76031ff17cb7a4b6b369e0205e6063e4edef8538
Martin.ruzicka <martin.ruzicka98@gmail.com>
2020-04-25 17:31:23 +0200

    Some irrelevant changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || config.get('PORT');
 
 const bot = new BootBot({
   accessToken: config.get('ACCESS_TOKEN'),
-  verifyToken: config.get('VERIFY_TOKEN'),
+  verifyToken: config.get('ahoj123'),
   appSecret: config.get('APP_SECRET')
 });
 
@@ -14,8 +14,22 @@ bot.on('message', (payload, chat) => {
 	console.log(`The user said: ${text}`);
 });
 
-bot.hear(['hello', 'hi'], (payload, chat) => {
-	console.log('The user said "hello" or "hi"!');
+bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
+  chat.say('Hi I am chatbot which shows you some stats of NHL teams or players.');
+  
 });
+
+bot.hear(['stat', 'stats', "info"], (payload, chat) => {
+  chat.say({
+    text: 'What stats do you want to show?',
+    buttons: [
+      { type: 'postback', title: 'Show teams stats', payload: 'TEAMS_STATS' },
+      { type: 'postback', title: 'Show players stats', payload: 'PLAYER_STAT' }
+    ]
+  });
+});
+
+//THIS should remember what was chosen and show some table with teams or players... and then user can click on player (for example) and chatbot shows a stats of him
+
 
 bot.start(port);
